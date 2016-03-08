489bfcdf95ae24662066636c928250bf2e1a9fb4
alexander <kovalas@yandex.ru>
2016-03-08 03:32:22 +0300

    Responsive chart

--- app/directives/ratingChart.js
+++ app/directives/ratingChart.js
@@ -1,5 +1,5 @@
 angular.module('dashboard').directive('ratingChart', function (numberOfPoints) {
-    var ctx, chart,
+    var ctx, chart, reportList,
         MONTH_LIST = 'jan. feb. mar. apr. may jun jul. aug. sep. oct. nov. dec.'.split(' ');
 
     function userFriendlyDate(reportItem) {
@@ -8,6 +8,7 @@ angular.module('dashboard').directive('ratingChart', function (numberOfPoints) {
         var date = new Date(reportItem.tillDate);
         return date.getDate() + ' ' + MONTH_LIST[date.getMonth()];
     }
+    userFriendlyDate.odd = false;
 
     function decorate(list) {
         list[0] = '...';
@@ -15,7 +16,31 @@ angular.module('dashboard').directive('ratingChart', function (numberOfPoints) {
         return list;
     }
 
-    userFriendlyDate.odd = false;
+    function drawChart(){
+        var data = {
+            labels: decorate(reportList.map(userFriendlyDate)),
+            datasets: [
+                {
+                    fillColor: "rgba(151,187,205,0)",
+                    strokeColor: "rgba(0,169,186,1)",
+                    pointColor: "rgba(0,169,186,1)",
+                    data: reportList.map(function (item) {
+                        return item.averageRating;
+                    })
+                }
+            ]
+        };
+
+        chart = new Chart(ctx).Line(data, {
+            scaleShowVerticalLines: false,
+            bezierCurve: false,
+            scaleOverride: true,
+            scaleSteps: 4,
+            scaleStepWidth: 1,
+            scaleStartValue: 1
+        });
+    }
+
     return {
         restrict: 'E',
         templateUrl: 'rating-chart.html',
@@ -26,31 +51,14 @@ angular.module('dashboard').directive('ratingChart', function (numberOfPoints) {
 
             ctx = document.getElementById('ratingChart').getContext('2d');
         },
-        controller: function ($scope, statisticData, StatisticTimelineReport) {
+        controller: function ($scope, $window, statisticData, StatisticTimelineReport) {
+            angular.element($window).bind('resize', function(){
+                chart.destroy();
+                drawChart();
+            });
             statisticData.then(function (data){
-                var reportList = StatisticTimelineReport.getTimelineReport(data.items, numberOfPoints);
-                var data = {
-                    labels: decorate(reportList.map(userFriendlyDate)),
-                    datasets: [
-                        {
-                            fillColor: "rgba(151,187,205,0)",
-                            strokeColor: "rgba(0,169,186,1)",
-                            pointColor: "rgba(0,169,186,1)",
-                            data: reportList.map(function (item) {
-                                return item.averageRating;
-                            })
-                        }
-                    ]
-                };
-
-                chart = new Chart(ctx).Line(data, {
-                    scaleShowVerticalLines: false,
-                    bezierCurve: false,
-                    scaleOverride: true,
-                    scaleSteps: 4,
-                    scaleStepWidth: 1,
-                    scaleStartValue: 1
-                });
+                reportList = StatisticTimelineReport.getTimelineReport(data.items, numberOfPoints);
+                drawChart();
             });
         }
     }
